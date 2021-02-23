650b9fccc269bb1639cf114ffc654873bdef8cc5
andreea1603 <62295059+andreea1603@users.noreply.github.com>
2021-02-23 22:50:40 +0200

    Add files via upload

---
+++ laborator2/Optional/src/Warehouse.java
@@ -0,0 +1,16 @@
+public class Warehouse extends Source{
+    String type;
+
+    public Warehouse(String names, int supply) {
+        this.type = "Warehouse";
+        this.supply = supply;
+        this.names = names;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
