de5fcdacaaebbe9d4049f2c97ba432f43a30cdeb
andreea1603 <62295059+andreea1603@users.noreply.github.com>
2021-03-22 00:31:22 +0200

    Add files via upload

--- laborator5/Optional/Compulsory/src/main/java/AddCommand.java
+++ laborator5/Optional/Compulsory/src/main/java/AddCommand.java
@@ -1,13 +1,33 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class AddCommand implements Command {
     Catalog catalog;
     @Override
     public void execute(String ... params) throws IOException{
 
-        for(String param: params){
-            catalog.add(new Song(param));
+        System.out.println("Introduceti tipul itemului(book/song)");
+        Scanner readCommand1 = new Scanner(System.in);
+        String type = readCommand1.nextLine();
+        System.out.println("Introduceti id-ul itemului");
+        String id = readCommand1.nextLine();
+        System.out.println("Introduceti numele itemului");
+        String name = readCommand1.nextLine();
+        System.out.println("Introduceti path-ul itemului");
+        String path = readCommand1.nextLine();
+        if (type.contains("book")) {
+            Book b1 = new Book(name, id);
+            b1.setLocation(path);
+            catalog.add(b1);
         }
-        ///////DACA E SONG SAU NU
+        if (type.contains("song")) {
+            Song s1 = new Song(name, id);
+            s1.setLocation(path);
+            catalog.add(s1);
+        }
+    }
+
+    public AddCommand(Catalog catalog) {
+        this.catalog = catalog;
     }
 }
